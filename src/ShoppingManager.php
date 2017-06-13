f7c0cbf4d7f9c5f4cb503f90b672b5e594b48f45
eneasdh-fs <eneasdh-fs@hotmail.com>
2017-06-13 20:37:34 +0200

    Refactoring the session manager

--- src/ShoppingManager.php
+++ src/ShoppingManager.php
@@ -9,6 +9,7 @@ namespace Enea\Cashier;
 use Enea\Cashier\Contracts\BuyerContract;
 use Enea\Cashier\Contracts\InvoiceContract;
 use Illuminate\Session\SessionManager;
+use Illuminate\Support\Collection;
 
 class ShoppingManager
 {
@@ -18,7 +19,13 @@ class ShoppingManager
      */
     protected $session;
 
-    protected const TOKEN_LENGTH = 20;
+    /**
+     * Main session key
+     *
+     * @var string
+     * */
+    protected  $key;
+
 
     /**
      * ShoppingManager constructor.
@@ -41,7 +48,11 @@ class ShoppingManager
     {
         $shopping = new ShoppingCard($buyer, $invoice);
 
-        $this->session->put($shopping->token(), $shopping );
+        if(! $this->isInitiated( ) ) {
+            $this->session->set($this->key(), collect());
+        }
+
+        $this->attach( $shopping );
 
         return $shopping;
     }
@@ -49,17 +60,87 @@ class ShoppingManager
     /**
      * Return the shopping cart from the session
      *
-     * @param $_token
+     * @param string $_token
      * @return ShoppingCard|null
      */
-    public function find( $_token ): ?ShoppingCard
+    public function find( string $_token ): ?ShoppingCard
     {
-        if($this->session->has($_token)) {
-            $this->session->get($_token);
+        if( ! $this->isInitiated( ) ) {
+            return null;
         }
 
-        return null;
+        return $this->carts( )->get( $_token ) ;
     }
 
+    /**
+     * Delete a specific shopping cart and return true if it was found
+     *
+     * @param string $_token
+     * @return bool
+     */
+    public function drop(string $_token ): bool
+    {
+        if( ! $this->isInitiated( ) ) {
+            return false;
+        }
 
+        $carts = $this->carts( );
+
+        if ( $has =  $carts->has( $_token ) ) {
+            $carts->forget( $_token );
+        }
+
+        return $has;
+    }
+
+    /**
+     * Delete all items from the session
+     *
+     * @return void
+     */
+    public function flush( ): void
+    {
+        $this->session->forget($this->key( ));
+    }
+
+    /**
+     * Add a new shopping cart to the session
+     *
+     * @param ShoppingCard $shopping
+     * @return void
+     */
+    protected function attach( ShoppingCard $shopping ): void
+    {
+        $this->carts()->put($shopping->token(), $shopping);
+    }
+
+    /**
+     * Returns the main key of the session
+     *
+     * @return string
+     */
+    protected function key( ): string
+    {
+        return $this->key ?: $this->key = config( 'cashier.session_key', 'default_laravel_shopping_session_key' );
+    }
+
+    /**
+     * Returns true in case the session handler has been initialized
+     *
+     * @return bool
+     */
+    protected function isInitiated( ): bool
+    {
+        return  $this->session->has( $this->key( ) );
+    }
+
+    /**
+     * Returns all shopping cars in session
+     *
+     * @return Collection|null
+     */
+    protected function carts( ): ? Collection
+    {
+        return $this->session->get($this->key( ));
+    }
 }
