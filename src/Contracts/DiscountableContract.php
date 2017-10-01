f58bb57669782c0f42b76229a5e75a45194f67b3
enea dhack <enea.so@live.com>
2017-10-01 16:02:20 -0500

    Update phpdocs

--- src/Contracts/DiscountableContract.php
+++ src/Contracts/DiscountableContract.php
@@ -5,15 +5,17 @@
 
 namespace Enea\Cashier\Contracts;
 
+use Illuminate\Support\Collection;
+
 /**
- * When implementing this interface, it is possible to assign a discount percentage on an item.
+ * When implementing this interface, it is possible to assign a discount on an item.
  */
 interface DiscountableContract
 {
     /**
      * Get the item discount in percentage.
      *
-     * @return int
+     * @return Collection<DiscountContract|int>
      */
-    public function getDiscountPercentage();
+    public function getDiscounts();
 }
