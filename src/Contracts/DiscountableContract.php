638796f04dc59557ce1ba1096d6dcb1c2b6b5a03
enea dhack <enea.so@live.com>
2017-10-02 15:21:37 -0500

    Change the return type of the DiscountableContract

--- src/Contracts/DiscountableContract.php
+++ src/Contracts/DiscountableContract.php
@@ -5,7 +5,7 @@
 
 namespace Enea\Cashier\Contracts;
 
-use Illuminate\Support\Collection;
+use Enea\Cashier\Modifiers\DiscountContract;
 
 /**
  * When implementing this interface, it is possible to assign a discount on an item.
@@ -15,7 +15,7 @@ interface DiscountableContract
     /**
      * Get the item discount in percentage.
      *
-     * @return Collection<DiscountContract|int>
+     * @return DiscountContract|null
      */
     public function getDiscounts();
 }
